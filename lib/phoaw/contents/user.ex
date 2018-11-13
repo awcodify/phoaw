d118c0b3e23e9bbf87a42020953e3f4b55fbb40b
Danu <awcodify@gmail.com>
2018-11-13 20:06:23 +0700

    add TODO docs

--- lib/phoaw/contents/user.ex
+++ lib/phoaw/contents/user.ex
@@ -1,4 +1,7 @@
 defmodule Phoaw.Contents.User do
+  @moduledoc """
+    TODO: add documentations here
+  """
   use Ecto.Schema
   import Ecto.Changeset
   import Comeonin.Bcrypt, only: [hashpwsalt: 1]
