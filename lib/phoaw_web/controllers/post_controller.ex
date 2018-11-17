75da42794e4049a44c238254af1da6eb69cf23b2
zuhri <zuhri@sleekr.co>
2018-11-17 12:59:34 +0700

    implement authorizer on post controller

--- lib/phoaw_web/controllers/post_controller.ex
+++ lib/phoaw_web/controllers/post_controller.ex
@@ -4,6 +4,8 @@ defmodule PhoawWeb.PostController do
   alias Phoaw.Contents
   alias Phoaw.Contents.Post
 
+  plug PhoawWeb.Plugs.Authorizer when action not in [:index, :show]
+
   def index(conn, _params) do
     posts = Contents.list_posts()
     render(conn, "index.html", posts: posts)
