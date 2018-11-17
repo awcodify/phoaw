cc05daf78ae7de57a92dde102d279300fbf1176b
zuhri <zuhri@sleekr.co>
2018-11-17 12:58:51 +0700

    add authorizer plugs

---
+++ lib/phoaw_web/plugs/authorizer.ex
@@ -0,0 +1,19 @@
+defmodule PhoawWeb.Plugs.Authorizer do
+  import Plug.Conn
+  use PhoawWeb, :controller
+
+  alias PhoawWeb.Plugs.Authorizer
+
+  def init(default), do: default
+
+  def call(conn, _default) do
+    current_user = get_session(conn, :current_user)
+    if current_user == nil do
+
+      conn
+        |> put_flash(:info, "You should log in first.")
+        |> redirect(to: Routes.session_path(conn, :new))
+    end
+    conn
+  end
+end
