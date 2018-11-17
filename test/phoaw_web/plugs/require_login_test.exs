cb81450aa798bce0668ddd3550838dbc14f9071c
zuhri <zuhri@sleekr.co>
2018-11-17 14:05:26 +0700

    add module docs

---
+++ test/phoaw_web/plugs/require_login_test.exs
@@ -0,0 +1,37 @@
+defmodule PhoawWeb.Plugs.RequireLoginTest do
+  use PhoawWeb.ConnCase
+  import Plug.Test
+
+  alias Phoaw.Contents.User
+  alias Phoaw.Repo
+
+  setup do
+    valid_params = %{
+      username: 'test',
+      password: 'test1234',
+      password_confirmation: 'test1234',
+      email: 'test@example.com'
+    }
+
+    %User{}
+    |> User.changeset(valid_params)
+    |> Repo.insert()
+  end
+
+  test "user is redirected when current_user is not set" do
+    conn = build_conn()
+    conn
+      |> PhoawWeb.Plugs.RequireLogin.call(%{})
+
+    assert redirected_to(conn) == Routes.session_path(conn, :new)
+  end
+
+  # test "user passes through when current_user is set" do
+  #   conn = build_conn()
+  #   conn
+  #     |> init_test_session(current_user: %User{})
+  #     |> PhoawWeb.Plugs.RequireLogin.call(%{})
+
+  #   assert
+  # end
+end
